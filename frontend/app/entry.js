949d5873b9c28a3906d597bc560d7d305b519409
Frazier Mork <fraziermork@gmail.com>
2016-06-09 13:05:45 -0700

    built out entry.js

---
+++ frontend/app/entry.js
@@ -0,0 +1,24 @@
+// REQUIRE ANGULAR
+require('angular');
+require('angular-router');
+
+// REQUIRE CSS
+require(`${__dirname}/css/base.css`);
+
+// REQUIRE SERVICES
+require(`${__dirname}/services/authentication.js`);
+require(`${__dirname}/services/reroute-check.js`);
+
+// REQUIRE COMPONENTS
+require(`${__dirname}/components/item/item-controller.js`);
+require(`${__dirname}/components/item/item-directive.js`);
+
+require(`${__dirname}/components/list/list-controller.js`);
+require(`${__dirname}/components/list/list-directive.js`);
+
+require(`${__dirname}/components/entry/entry-controller.js`);
+
+require(`${__dirname}/components/all/all-controller.js`);
+
+// REQUIRE MAIN APP 
+require(`${__dirname}/main/main-controller.js`);
